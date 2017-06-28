f83a6e3006d97db4d317a4eff1f02a16defd3d57
Zhmoll <zhmoll@zhmoll.com>
2017-06-28 13:52:05 +0800

    不用传参

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(session({
 
 require('./routes')(app);
 require('./routes/wechat')(app);
-require('./lib/websocket')(app);
+require('./lib/websocket');
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
