71b03d1c334a6457ce6980b7531742fc3fc2ec1e
Zhmoll <zhmoll@zhmoll.com>
2017-07-01 13:27:58 +0800

    添加用户查询自己的弹幕记录功能

--- routes/wechat.js
+++ routes/wechat.js
@@ -7,6 +7,7 @@ const Signin = require('../model/signin');
 const Danmaku = require('../model/danmakus');
 const Room = require('../model/rooms');
 const wsDanmaku = require('../lib/websocket');
+const moment = require('moment');
 
 const mw = wechat(config)
   .text((message, req, res, next) => {
@@ -19,6 +20,12 @@ const mw = wechat(config)
   })
   .event((message, req, res, next) => {
     switch (message.Event) {
+      case 'click': {
+        switch (message.EventKey) {
+          case 'danmaku_histroy':
+            return danmaku_histroy(message, req, res);
+        }
+      }
       case 'subscribe':
         return subscribe_helper(message, req, res);
       case 'scancode_waitmsg': {
@@ -88,8 +95,13 @@ function danmaku_helper(message, req, res) {
         res.reply('发送失败，请确认是否为该课堂成员！');
         return;
       }
-      await Danmaku.create({ student: req.wxsession.userid, content: content, room: roomid });
-      wsDanmaku[roomid].ws.send(JSON.stringify({ type: 'danmaku', body: { uid, name, content } }), console.error);
+      if (content.length > 50) {
+        res.reply('发送失败，请缩短篇幅！');
+        return;
+      }
+      const blocked = false;
+      await Danmaku.create({ student: req.wxsession.userid, content, room: roomid, blocked });
+      wsDanmaku[roomid].ws.send(JSON.stringify({ type: 'danmaku', body: { uid, name, content, blocked } }), console.error);
       res.reply('发送成功！');
     }
     else {
@@ -113,6 +125,41 @@ function subscribe_helper(message, req, res) {
     else {
       res.reply(`欢迎使用弹幕课堂！请绑定学生信息，格式为绑定+学号+姓名，例如：'绑定+12345678+张三'`);
     }
+  })().catch(e => console.error(e));
+}
+
+// 弹幕历史
+function danmaku_histroy(message, req, res) {
+  (async () => {
+    // 确保登录 - start
+    let student;
+    if (!req.wxsession.uid) {
+      student = await Student.findOne({ openid: message.FromUserName });
+      if (!student) {
+        res.reply(`你还未绑定账号！格式为绑定+学号+姓名，例如：'绑定+12345678+张三'`);
+        return;
+      }
+      req.wxsession.userid = student.id;
+      req.wxsession.uid = student.uid;
+      req.wxsession.name = student.name;
+    }
+    // 确保登录 - end
+
+    const danmakus = await Danmaku
+      .find({ student: student.id }, '-_id content createdAt room', { limit: 7 })
+      .sort('-createdAt')
+      .populate({
+        path: 'room',
+        select: 'title'
+      });
+    const result = [{ title: '弹幕课堂 - 最近七条用户弹幕记录' }];
+    danmakus.forEach((danmaku) => {
+      danmaku.createdAt = moment(danmaku.createdAt).utcOffset(8).format('YYYY-MM-DD HH:mm:ss');
+      result.push({
+        title: `内容：${danmaku.content}\n房间：${danmaku.room.title}\n时间：${danmaku.createdAt}`
+      });
+    });
+    res.reply(result);
   })().catch(e => console.error(e));
 }
 
