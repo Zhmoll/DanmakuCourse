dd4f67e76049f6482dcca601d346a61a1860d08f
Zhmoll <zhmoll@zhmoll.com>
2017-07-02 20:27:49 +0800

    打印客户端post过来的数据

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 module.exports = (app) => {
   app.use('/wechat', require('./wechat'));
+  app.use('/', (req, res, next) => {
+    console.log(req.body);
+    next();
+  });
   app.use('/teachers', require('./teacher'));
 };
